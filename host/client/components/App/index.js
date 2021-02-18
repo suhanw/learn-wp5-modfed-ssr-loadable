b19808056a6e695650b22aeee6012ae63f3d659b
Suhan Wijaya <suhanw@gmail.com>
2021-02-18 16:26:35 -0500

    use loadable to load TopNav

--- host/client/components/App/index.js
+++ host/client/components/App/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
+import loadable from '@loadable/component';
 import style from './style';
 import TopNav from 'remote/top-nav';
+// const TopNav = loadable(() => import('remote/top-nav'), { ssr: true });
 
 const App = () => {
 	useEffect(() => {
@@ -14,6 +16,5 @@ const App = () => {
 		</div>
 	);
 }
-
 
 export default App; 
